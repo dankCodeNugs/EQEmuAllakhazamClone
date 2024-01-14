f93438384e1cc98f9ab6726deef6da389755b5cc
proxeeus <proxeeus@gmail.com>
2024-01-14 15:56:53 +0100

    update player bots documentation

--- pages/player_bots/player_bots_trading.php
+++ pages/player_bots/player_bots_trading.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html>
 
-<h2>Buying items from Player Bots</h2>
+<h2>Player Bots Trading</h2>
 This section lists everything Player Bots are able to sell to players, from faction items to certain quest items (to accomodate to the solo nature of the project) or special pieces of equipment, such as jewelry.
-Simply click on the item you'd like to buy from a Player Bot below to have the line copied to your clipboard, ready to be pasted in your EverQuest session!
+A "pseudo-Multiquest" system is also in place for Velious Faction Armor pieces. It is then possible to "sell" the base items to Player Bots for certain profit.
+
+Simply click on the item you'd like to buy / MQ from/to a Player Bot below to have the line copied to your clipboard, ready to be pasted in your EverQuest session!
 
 <h2 id="toc">List of purchasable items</h2>
 <ol>
@@ -13,6 +15,7 @@ Simply click on the item you'd like to buy from a Player Bot below to have the l
   <li><a href="#misc-jewelry">Misc. Jewelry</a></li>
   <li><a href="#velious-gems">Velious Gems</a></li>
   <li><a href="#velious-faction">Velious Faction</a></li>
+  <li><a href="#velious-armor">Velious Faction Armor MQs</a></li>
 </ol>
 
     <h3 id="misc-tradeskills">Misc. Tradeskills</h3>
@@ -62,6 +65,119 @@ Simply click on the item you'd like to buy from a Player Bot below to have the l
     <p>coldain head</p>
     <a href="#toc">Back to top</a>
 
+    <h3 id="velious-armor">Velious Faction Armor MQs</h3>
+    <h4>Thurgadin</h4>
+    <h4>Plate</h4>
+    <p>corroded breastplate</p>
+    <p>corroded plate helmet</p>
+    <p>corroded plate boots</p>
+    <p>corroded plate bracer</p>
+    <p>corroded plate gauntlets"</p>
+    <p>corroded plate greaves</p>
+    <p>corroded plate vambraces</p>
+    <a href="#toc">Back to top</a>
+    <h4>Chain</h4>
+    <p>corroded chain tunic</p>
+    <p>corroded chain boots</p>
+    <p>corroded chain bracer</p>
+    <p>corroded chain coif</p>
+    <p>corroded chain gauntlets</p>
+    <p>corroded chain leggings</p>
+    <p>corroded chain sleeves</p>
+    <a href="#toc">Back to top</a>
+    <h4>Leather</h4>
+    <p>eroded leather boots</p>
+    <p>eroded leather bracelet</p>
+    <p>eroded leather cap</p>
+    <p>eroded leather gloves</p>
+    <p>eroded leather leggings</p>
+    <p>eroded leather sleeves</p>
+    <p>eroded leather tunic</p>
+    <a href="#toc">Back to top</a>
+    <h4>Cloth</h4>
+    <p>torn enchanted silk boots</p>
+    <p>torn enchanted silk gloves</p>
+    <p>torn enchanted silk pantaloons</p>
+    <p>torn enchanted silk robe</p>
+    <p>torn enchanted silk sleeves</p>
+    <p>torn enchanted silk turban</p>
+    <p>torn enchanted silk wristband</p>
+    <a href="#toc">Back to top</a>
+    <h4>Kael</h4>
+    <h4>Plate</h4>
+    <p>ancient tarnished breastplate</p>
+    <p>ancient tarnished plate boots</p>
+    <p>ancient tarnished plate bracelet</p>
+    <p>ancient tarnished gauntlets</p>
+    <p>ancient tarnished helmet</p>
+    <p>ancient tarnished vambraces</p>
+    <p>ancient tarnished greaves</p>
+    <a href="#toc">Back to top</a>
+    <h4>Chain</h4>
+    <p>ancient tarnished chain boots</p>
+    <p>ancient tarnished chain bracer</p>
+    <p>ancient tarnished chain coif</p>
+    <p>ancient tarnished chain gauntlets</p>
+    <p>ancient tarnished chain leggings</p>
+    <p>ancient tarnished chain sleeves</p>
+    <p>ancient tarnished chain tunic</p>
+    <a href="#toc">Back to top</a>
+    <h4>Leather</h4>
+    <p>ancient leather boots</p>
+    <p>ancient leather bracelet</p>
+    <p>ancient leather cap</p>
+    <p>ancient leather gloves</p>
+    <p>ancient leather leggings</p>
+    <p>ancient leather sleeves</p>
+    <p>ancient leather tunic</p>
+    <a href="#toc">Back to top</a>
+    <h4>Silk</h4>
+    <p>ancient silk robe</p>
+    <p>ancient silk boots</p>
+    <p>ancient silk gloves</p>
+    <p>ancient silk pantaloons</p>
+    <p>ancient silk sleeves</p>
+    <p>ancient silk turban</p>
+    <p>ancient silk wristband</p>
+    <a href="#toc">Back to top</a>
+    <h4>Skyshrine</h4>
+    <h4>Plate</h4>
+    <p>unadorned plate boots</p>
+    <p>unadorned plate bracer</p>
+    <p>unadorned plate gauntlets</p>
+    <p>unadorned plate greaves</p>
+    <p>unadorned plate helmet</p>
+    <p>unadorned plate vambraces</p>
+    <p>unadorned breastplate</p>
+    <a href="#toc">Back to top</a>
+    <h4>Chain</h4>
+    <p>unadorned chain boots</p>
+    <p>unadorned chain bracer</p>
+    <p>unadorned chain coif</p>
+    <p>unadorned chain gauntlets</p>
+    <p>unadorned chain leggings</p>
+    <p>unadorned chain sleeves</p>
+    <p>unadorned chain tunic</p>
+    <a href="#toc">Back to top</a>
+    <h4>Leather</h4>
+    <p>unadorned leather boots</p>
+    <p>unadorned leather bracelet</p>
+    <p>unadorned leather cap</p>
+    <p>unadorned leather gloves</p>
+    <p>unadorned leather leggings</p>
+    <p>unadorned leather sleeves</p>
+    <p>unadorned leather tunic</p>
+    <a href="#toc">Back to top</a>
+    <h4>Silk</h4>
+    <p>tattered silk boots</p>
+    <p>tattered silk gloves</p>
+    <p>tattered silk pantaloons</p>
+    <p>tattered silk robe</p>
+    <p>tattered silk sleeves</p>
+    <p>tattered silk turban</p>
+    <p>tattered silk wristband</p>
+    <a href="#toc">Back to top</a>
+
     <!-- Leave this here -->
 	<script>
 		let paragraphs = document.getElementsByTagName("p");
@@ -70,7 +186,21 @@ Simply click on the item you'd like to buy from a Player Bot below to have the l
 			paragraphs[i].addEventListener("click", function()
             {
 				let text = this.textContent;
-				navigator.clipboard.writeText("buy a " + text)
+                let prefix = "";
+                if(text.includes("corroded")  || text.includes("torn enchanted") || text.includes("tattered silk"))
+                {
+                    prefix = "multiquest a ";
+                }
+                else if(text.includes("ancient tarnished") || text.includes("ancient leather") || text.includes("ancient silk") || text.includes("eroded")
+                || text.includes("unadorned"))
+                {
+                    prefix = "multiquest an ";
+                }
+                else
+                {
+                    prefix = "buy a ";
+                }
+				navigator.clipboard.writeText(prefix + text)
 					.then(() => console.log(`Copied "${text}" to clipboard!`))
 					.catch((error) => console.error(`Failed to copy "${text}" to clipboard: ${error}`));
 			});
